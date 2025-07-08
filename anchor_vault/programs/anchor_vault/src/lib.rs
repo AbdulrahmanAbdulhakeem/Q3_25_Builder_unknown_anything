df8fe3e67649fe3083af4d7e23236ce7e7c677a8
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2025-07-09 00:39:28 +0100

    Implemented trait on the Payment acoounts

--- anchor_vault/programs/anchor_vault/src/lib.rs
+++ anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,7 +1,10 @@
 #![allow(unexpected_cfgs)]
 #![allow(deprecated)]
 
-use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
 
 declare_id!("34AuAC3qLVb5uBSgfTSPea9Q2zcuWf2SQFMjQY2sm3Lg");
 
@@ -10,15 +13,14 @@ pub mod anchor_vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+        ctx.accounts.initialize(&ctx.bumps)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize <'info>{
+pub struct Initialize<'info> {
     #[account(mut)]
-    pub user:Signer<'info>,
+    pub user: Signer<'info>,
     #[account(
         init,
         payer = user,
@@ -26,21 +28,68 @@ pub struct Initialize <'info>{
         seeds = [b"vaultState" , user.key().as_ref()],
         bump
     )]
-    pub vault_state:Account<'info,VaultState>,
+    pub vault_state: Account<'info, VaultState>,
     #[account(
         mut,
         seeds = [b"vault", vault_state.key().as_ref()],
         bump
     )]
-    pub vault:SystemAccount<'info>,
-    pub system_program:Program<'info,System>
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-impl<'info>Initialize<'info> {
-    pub fn initialize (&mut self, amount:u64) -> Result<()> {
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bump: &InitializeBumps) -> Result<()> {
+        let rent_exempt = Rent::get()?.minimum_balance(self.vault.to_account_info().data_len());
+
+        let user_balance = self.user.lamports();
+        require_gte!(
+            user_balance,
+            rent_exempt,
+            VaultError::InsufficientUserBalance
+        );
+
         let cpi_program = self.system_program.to_account_info();
 
-        let cpi_accounts = Transfer{
+        let cpi_accounts = Transfer {
+            from: self.user.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, rent_exempt)?;
+
+        self.vault_state.vault_bump = bump.vault;
+        self.vault_state.state_bump = bump.vault_state;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Payment<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        seeds = [b"vaultState" , user.key().as_ref()],
+        bump,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Payment<'info> {
+    pub fn deposit(&self, amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
             from:self.user.to_account_info(),
             to:self.vault.to_account_info()
         };
@@ -49,36 +98,41 @@ impl<'info>Initialize<'info> {
 
         transfer(cpi_ctx, amount)
     }
+
+    pub fn withdraw(&self,amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer{
+            from:self.vault.to_account_info(),
+            to:self.user.to_account_info()
+        };
+
+        let seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump]
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, amount)
+    }
 }
-
-
-#[derive(Accounts)]
-pub struct Payment<'info> {
-    #[account(mut)]
-    pub user:Signer<'info>,
-    #[account(
-        seeds = [b"vaultState" , user.key().as_ref()],
-        bump,
-    )]
-    pub vault_state:Account<'info,VaultState>,
-    #[account(
-        mut,
-        seeds = [b"vault", vault_state.key().as_ref()],
-        bump
-    )]
-    pub vault:SystemAccount<'info>,
-    pub system_program:Program<'info,System>
-}
-
-impl<'info>Payment<'info>{}
-
 
 #[account]
 pub struct VaultState {
-    pub vault_bump:u8,
-    pub state_bump:u8,
+    pub vault_bump: u8,
+    pub state_bump: u8,
 }
 
 impl Space for VaultState {
-    const INIT_SPACE: usize = 8 + 1*2;
+    const INIT_SPACE: usize = 8 + 1 * 2;
+}
+
+#[error_code]
+pub enum VaultError {
+    #[msg("Insufficient user balance")]
+    InsufficientUserBalance
 }
