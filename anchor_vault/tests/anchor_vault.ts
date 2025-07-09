094d101e2d3ffb0c94d37be94dfc78f8d5bd5d2c
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2025-07-09 09:47:10 +0100

    deposit sol test

--- anchor_vault/tests/anchor_vault.ts
+++ anchor_vault/tests/anchor_vault.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
 import { expect } from "chai";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 describe("anchor_vault", () => {
   // Configure the client to use the local cluster.
@@ -19,27 +20,50 @@ describe("anchor_vault", () => {
   )[0];
 
   const vault = anchor.web3.PublicKey.findProgramAddressSync(
-    [
-      Buffer.from("vault"),
-      vaultState.toBuffer()
-    ],
+    [Buffer.from("vault"), vaultState.toBuffer()],
     program.programId
   )[0];
 
   it("should initialize vault!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().accountsPartial({
-      user:provider.wallet.publicKey,
-      vaultState,
-      vault,
-      systemProgram:anchor.web3.SystemProgram.programId
-    }).rpc();
+    const tx = await program.methods
+      .initialize()
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
 
     console.log(`Transaction signature:${tx}`);
 
-    const vaultStateAccount = await program.account.vaultState.fetch(vaultState);
+    const vaultStateAccount = await program.account.vaultState.fetch(
+      vaultState
+    );
 
     expect(vaultStateAccount.stateBump).to.be.a("number");
     expect(vaultStateAccount.vaultBump).to.be.a("number");
+  });
+
+  it("should deposit Sol", async () => {
+    const depositAmount = 0.2 * LAMPORTS_PER_SOL;
+    const initialAmt = await provider.connection.getBalance(vault);
+
+    const tx = await program.methods
+      .deposit(new anchor.BN(depositAmount))
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log(`Transaction signature:${tx}`);
+
+    const finalAmt = await provider.connection.getBalance(vault);
+
+    expect(finalAmt - initialAmt).to.be.equal(depositAmount);
   });
 });
