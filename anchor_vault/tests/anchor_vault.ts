673e27a43d020bb58dc765ceffdf98f730e652b5
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2025-07-09 09:27:14 +0100

    added initialize vault test

--- anchor_vault/tests/anchor_vault.ts
+++ anchor_vault/tests/anchor_vault.ts
@@ -1,16 +1,45 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
+import { expect } from "chai";
 
 describe("anchor_vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  // space = VaultState::INIT_SPACE,
+  //       seeds = [b"vaultState" , user.key().as_ref()],
 
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
 
-  it("Is initialized!", async () => {
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vaultState"), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  )[0];
+
+  const vault = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("vault"),
+      vaultState.toBuffer()
+    ],
+    program.programId
+  )[0];
+
+  it("should initialize vault!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accountsPartial({
+      user:provider.wallet.publicKey,
+      vaultState,
+      vault,
+      systemProgram:anchor.web3.SystemProgram.programId
+    }).rpc();
+
+    console.log(`Transaction signature:${tx}`);
+
+    const vaultStateAccount = await program.account.vaultState.fetch(vaultState);
+
+    expect(vaultStateAccount.stateBump).to.be.a("number");
+    expect(vaultStateAccount.vaultBump).to.be.a("number");
   });
 });
