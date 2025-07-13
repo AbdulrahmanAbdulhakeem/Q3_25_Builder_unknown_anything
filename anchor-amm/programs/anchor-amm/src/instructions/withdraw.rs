db1425b18cae91592975f3cd1bb1661ff53c93b2
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2025-07-14 00:35:14 +0100

    Implemented the withdraw instruction

--- anchor-amm/programs/anchor-amm/src/instructions/withdraw.rs
+++ anchor-amm/programs/anchor-amm/src/instructions/withdraw.rs
@@ -1,17 +1,15 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{
-    associated_token::AssociatedToken,
-    token_interface::{
-        mint_to, transfer_checked, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked,
-    },
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{
+    burn, transfer_checked, Burn, Mint, TokenAccount, TokenInterface, TransferChecked,
 };
-use constant_product_curve::ConstantProduct;
+use constant_product_curve::{ConstantProduct, XYAmounts};
 
 use crate::error::AmmError;
-use crate::Config;
+use crate::state::Config;
 
 #[derive(Accounts)]
-pub struct Deposit<'info> {
+pub struct Withdraw<'info> {
     #[account(mut)]
     pub lp_provider: Signer<'info>,
     pub mint_x: InterfaceAccount<'info, Mint>,
@@ -19,12 +17,12 @@ pub struct Deposit<'info> {
     #[account(
         has_one = mint_x,
         has_one = mint_y,
-        seeds = [b"config", mint_x.key().to_bytes().as_ref(),mint_y.key().to_bytes().as_ref(),config.seed.to_be_bytes().as_ref()],
+        seeds = [b"config",mint_x.key().to_bytes().as_ref(),mint_y.key().to_bytes().as_ref(),config.seed.to_be_bytes().as_ref()],
         bump = config.config_bump
     )]
     pub config: Account<'info, Config>,
     #[account(
-        seeds = [b"lp",config.key().as_ref()],
+        seeds = [b"lp", config.key().as_ref()],
         bump = config.lp_bump,
         mint::decimals = 6,
         mint::authority = config
@@ -62,86 +60,59 @@ pub struct Deposit<'info> {
         init_if_needed,
         payer = lp_provider,
         associated_token::mint = mint_lp,
-        associated_token::authority = mint_lp,
+        associated_token::authority = lp_provider,
         associated_token::token_program = token_program
     )]
     pub lp_provider_ata_lp: InterfaceAccount<'info, TokenAccount>,
-    pub system_program: Program<'info, System>,
-    pub token_program: Interface<'info, TokenInterface>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
 }
 
-impl<'info> Deposit<'info> {
-    pub fn deposit(&mut self, max_y: u64, max_x: u64, lp_amount: u64) -> Result<()> {
-        // lp_amount: amount of LP Tokens that the user wants to claim
-        // max_x: the max amount of x they are willing to deposit
-        // max_y: the max amount of y they are willing to deposit
-
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, lp_amount: u64, min_x: u64, min_y: u64) -> Result<()> {
         require!(lp_amount > 0, AmmError::InvalidAmount);
         require!(!self.config.locked, AmmError::AMMLocked);
 
-        let (x, y) = match self.mint_lp.supply == 0
-            && self.vault_x.amount == 0
-            && self.vault_y.amount == 0
-        {
-            true => (max_x, max_y),
-            false => {
-                let amount = ConstantProduct::xy_deposit_amounts_from_l(
-                    self.vault_x.amount,
-                    self.vault_y.amount,
-                    self.mint_lp.supply,
-                    lp_amount,
-                    6,
-                ).unwrap();
-                (amount.x,amount.y)
-            }
-        };
+        let xy_amounts = ConstantProduct::xy_withdraw_amounts_from_l(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.mint_lp.supply,
+            lp_amount,
+            6,
+        ).unwrap();
 
-        require!(max_x >= x,AmmError::InsufficientTokenX);
-        require!(max_y >= y,AmmError::InsufficientTokenY);
+        require!(min_x <=xy_amounts.x,AmmError::InsufficientTokenX);
+        require!(min_y <= xy_amounts.y,AmmError::InsufficientTokenY);
 
-        self.deposit_tokens(true, x)?;
-        self.deposit_tokens(false, y)?;
-        self.mint_lp_token(lp_amount)?;
-        Ok(())
+        self.withdraw_tokens(true, xy_amounts.x)?;
+        self.withdraw_tokens(false, xy_amounts.y)?;
+
+        self.burn_lp_tokens(lp_amount)
     }
 
-    fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+    fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
         let cpi_program = self.token_program.to_account_info();
 
         let (cpi_accounts, mint_decimals) = match is_x {
             true => (
                 TransferChecked {
-                    from: self.lp_provider_ata_x.to_account_info(),
+                    from: self.vault_x.to_account_info(),
+                    to: self.lp_provider_ata_x.to_account_info(),
                     mint: self.mint_x.to_account_info(),
-                    to: self.vault_x.to_account_info(),
-                    authority: self.lp_provider.to_account_info(),
+                    authority: self.config.to_account_info(),
                 },
                 self.mint_x.decimals,
             ),
             false => (
                 TransferChecked {
-                    from: self.lp_provider_ata_y.to_account_info(),
+                    from: self.vault_y.to_account_info(),
+                    to: self.lp_provider_ata_y.to_account_info(),
                     mint: self.mint_y.to_account_info(),
-                    to: self.vault_y.to_account_info(),
-                    authority: self.lp_provider.to_account_info(),
+                    authority: self.config.to_account_info(),
                 },
                 self.mint_y.decimals,
             ),
-        };
-
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-
-        transfer_checked(cpi_ctx, amount, mint_decimals)
-    }
-
-    fn mint_lp_token(&mut self, amount: u64) -> Result<()> {
-        let cpi_program = self.token_program.to_account_info();
-
-        let cpi_accounts = MintTo {
-            mint: self.mint_lp.to_account_info(),
-            to: self.lp_provider_ata_lp.to_account_info(),
-            authority: self.config.to_account_info(),
         };
 
         let mint_x = self.mint_x.key().to_bytes();
@@ -154,6 +125,20 @@ impl<'info> Deposit<'info> {
 
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
 
-        mint_to(cpi_ctx, amount)
+        transfer_checked(cpi_ctx, amount, mint_decimals)
+    }
+
+    fn burn_lp_tokens(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Burn {
+            mint: self.mint_lp.to_account_info(),
+            from: self.lp_provider_ata_lp.to_account_info(),
+            authority: self.lp_provider.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        burn(cpi_ctx, amount)
     }
 }
